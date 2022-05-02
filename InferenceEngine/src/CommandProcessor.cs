8ee3a517b215b934c02355f38e2eb63a817da038
Ruan Swanepoel <ruane.swanepoel@gmail.com>
2022-05-02 15:18:37 +1000

    1

---
+++ InferenceEngine/src/CommandProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InferenceEngine {
+
+	class CommandProcessor {
+
+		public void execute(string[] args) {
+
+		}
+
+	}
+
+}
